d504a5928113435df69985f9fdb9e7729f324491
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-09-12 20:37:34 -0700

    feat: Estilos en linea en JSX

--- 2-intro-react/espacios-expresiones/src/main.jsx
+++ 2-intro-react/espacios-expresiones/src/main.jsx
@@ -1,25 +1,24 @@
-import { StrictMode } from 'react'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const username = 'Armando'
+const username = "Armando";
 
 /* const element = (
   <h1>Hola {username} {username} String de texto</h1>
 ) */
 
-const shoppingList = ['manzanas', 'platanos', 'melón', 'mango', 'sandía']
+const shoppingList = ["manzanas", "platanos", "melón", "mango", "sandía"];
 // Esto es JS (afuera de los elementos)
 
 //  const element = ( //JSX, dentro de los paréntesis
 //   <p>No de {/* HOLA! */} cosas restantes por comprar: {shoppingList.length} </p>
-// ) 
+// )
 
-const mi_id = 'input-texto'
+/* const mi_id = 'input-texto'
 const element = (
   <input id={mi_id} required={true} type='text'></input>
-)
-
+) */
 
 /* const element = React.createElement(
   'h1',
@@ -31,6 +30,35 @@ const element = (
   mi_id
 ) */
 
-createRoot(document.getElementById('root')).render(
-  element
-)
+/*
+ * Agregar estilos en línea
+ */
+/* 
+
+const pStyles = {
+  backgroundColor: "goldenrod",
+  fontSize: "1rem",
+  fontFamily: "sans-serif",
+  textAlign: "center",
+};
+const element = (
+  <p style={pStyles}>
+    No. de cosas restantes por comprar: {shoppingList.length}
+  </p>
+); 
+*/
+
+const element = (
+  <p
+    style={{
+      backgroundColor: "goldenrod",
+      fontSize: "1rem",
+      fontFamily: "sans-serif",
+      textAlign: "center",
+    }}
+  >
+    No. de cosas restantes por comprar: {shoppingList.length}
+  </p>
+);
+
+createRoot(document.getElementById("root")).render(element);
